f921be802924bc82de917ad4613c5c0e4d82a9ba
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2025-06-21 20:15:52 -0700

    Feat: add Vengeance component

---
+++ src/Greeting.jsx
@@ -0,0 +1,9 @@
+function Portrait () {
+  return <img src="ai-batman.jpg" alt="portrait of batman" />
+}
+
+function Vengeance() {
+  return <h1>&quot;I am Vengeance!&quot;</h1>;
+}
+
+export { Portrait, Vengeance };
